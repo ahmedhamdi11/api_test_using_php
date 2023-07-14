94ff60213f0b26e1c72a0b3a4b753246953be482
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-07-14 19:19:01 +0300

    updtate note with image

--- notes/update_note.php
+++ notes/update_note.php
@@ -7,9 +7,15 @@
     $note_id =postRequest('note_id');
     $note_title =postRequest('note_title');
     $note_content =postRequest('note_content');
+    $note_image =postRequest('note_image');
+    
+    if(isset($_FILES['image'])){
+        deleteImage('../uploaded_images/',$note_image);
+        $note_image =uploadImage('image');
+    }
 
-    $stmt =  $connect->prepare("UPDATE `notes` SET `note_title` =? , `note_content`=? WHERE `note_id`=?");
-    $stmt->execute(array($note_title,$note_content,$note_id));
+    $stmt =  $connect->prepare("UPDATE `notes` SET `note_title` =? , `note_content`=? ,`note_image`=? WHERE `note_id`=?");
+    $stmt->execute(array($note_title, $note_content, $note_image, $note_id));
 
     $count = $stmt->rowCount();
 
